fb10f180e3f32b6d6fa7834d12578b4f80e5a006
hanqunfeng <hanqf2008@163.com>
2026-09-23 15:05:49 +0800

    2026-09-23 15:05:49#增加短参数

--- Formula/color_echo.rb
+++ Formula/color_echo.rb
@@ -1,8 +1,8 @@
 class ColorEcho < Formula
   desc "Print colorful text in terminal"
   homepage "https://github.com/hanqunfeng/color_echo"
-  url "https://github.com/hanqunfeng/color_echo/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "1d923c929ef8e24b2da8e4f48903facaaeccc12772a3442d7c1758b98cdf1e9a"
+  url "https://github.com/hanqunfeng/color_echo/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "c06e2f0f7cd0b0a7e33f6d6631173112d2ffeedf08d2fa65e4d60cf0885dd313"
   license "MIT"
 
   def install
